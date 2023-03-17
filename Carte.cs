07fea6764dc7744d18acf33ae4368d779d0f6744
Andries George Șerban <george.andries@student.usv.ro>
2023-03-17 22:43:53 +0200

    LAB_3- citire si afisare date de la tastatura init

--- Carte.cs
+++ Carte.cs
@@ -8,17 +8,88 @@ namespace Lab2_Tema
 {
     internal class Carte
     {
+        private const char SEPARATOR = ';';
         public string Titlu { get; set; }
         public string Autor { get; set; }
         public int AnPublicatie { get; set; }
         public bool Valabilitate { get; set; }
+        public string Detinator { get; set; }
 
-        public Carte(string titlu, string autor, int anPublicatie, bool valabilitate)
+        public Carte()
+        {
+            Titlu = Autor = Detinator = string.Empty;
+            AnPublicatie = 0;
+            Valabilitate = false;
+        }
+        public Carte(string titlu, string autor, int anPublicatie, bool valabilitate, string detinator)
         {
             Titlu = titlu;
             Autor = autor;
             AnPublicatie = anPublicatie;
             Valabilitate = valabilitate;
+            Detinator = detinator;
+        }
+
+        public static Carte CitesteCarteTastatura() ///// LAB_3 - citirea datelor de la tastatura;
+        {
+            Console.WriteLine("Introduceti titlul cartii:");
+            string titlu = Console.ReadLine();
+
+            Console.WriteLine("Introduceti autorul cartii:");
+            string autor = Console.ReadLine();
+
+            Console.WriteLine("Introduceti anul publicarii:");
+            int anPublicatie = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti valabilitatea cartii (true/false):");
+            bool valabilitate = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti detinatorul cartii:");
+            string detinator = Console.ReadLine();
+
+            return new Carte(titlu, autor, anPublicatie, valabilitate, detinator);
+        }
+
+        public void CautaCarte(string criteriu)
+        {
+
+        }
+
+        public void AfiseazaCarte() ///// LAB_3 - afisarea datelor de la tastatura;
+        {
+            string stringPentruAfisare = string.Format("Informatiile despre carte sunt urmatoarele:\n" +
+                "Titlu: {0}\tAutor: {1}\tAn Publicatie: {2}\tValabilitate: {3}\tDetinator: {4}",
+                GetTitlu(),
+                GetAutor(),
+                GetAnPublicatie(),
+                GetValabilitate(),
+                GetDetinator());
+            Console.WriteLine(stringPentruAfisare);
+        }
+
+        public string GetTitlu()
+        {
+            return Titlu;
+        }
+
+        public string GetAutor()
+        {
+            return Autor;
+        }
+
+        public int GetAnPublicatie()
+        {
+            return AnPublicatie;
+        }
+
+        public bool GetValabilitate()
+        {
+            return Valabilitate;
+        }
+
+        public string GetDetinator()
+        { 
+            return Detinator;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,16 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Lab2_Tema
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Carte carte = new Carte();
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            int nrCarti = 0;
 
 
-            Carte carte = Carte.CitesteCarte();
+            string optiune;
+            do
+            {
+                Console.WriteLine("I. Introducere informatii pentru carte de la tastatura");
+                Console.WriteLine("A. Afisare ultimele informatii intruduse");
+                //Console.WriteLine("F. Afisare studenti din fisier");
+                //Console.WriteLine("S. Salvare student in fisier");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "I":
+                        carte = Carte.CitesteCarteTastatura();
+
+                        break;
+                    case "A":
+                        carte.AfiseazaCarte();
+
+                        break;
+/*                    case "F":
+                        Student[] studenti = adminStudenti.GetStudenti(out nrStudenti);
+                        AfisareStudenti(studenti, nrStudenti);
+
+                        break;*/
+/*                    case "S":
+                        int idStudent = nrStudenti + 1;
+                        student.SetIdStudent(idStudent);
+                        //adaugare student in fisier
+                        adminStudenti.AddStudent(student);
+
+                        nrStudenti = nrStudenti + 1;
+
+                        break;*/
+                    case "X":
+
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
         }
     }
 }
